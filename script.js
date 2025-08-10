08d5c583cbff7eccb9d441cfbdc37770474d968a
Ramzin007 <muhammedramzinp0@gmail.com>
2025-08-10 23:28:18 +0530

    updated

--- script.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-
-const app = express();
-
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static("public"));
