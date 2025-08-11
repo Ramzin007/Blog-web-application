b3cca1a597348fb328bcb2a3fa81203fcc268da8
Ramzin007 <muhammedramzinp0@gmail.com>
2025-08-12 00:38:48 +0530

    update header.ejs

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home", { title: "Home" });
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About" });
+});
+
+app.get("/search", (req, res) => {
+    res.render("search", { title: "search" });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
 });
